4d03b8c3cd393bf87ccb0be50711015d5504a245
desahtunefrequency <112801737+desahtunefrequency@users.noreply.github.com>
2024-10-08 20:28:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -124,11 +124,12 @@ if exercise_type == "Standardna konverzija":
             st.error(f"Netočno. Točan odgovor je Dsph: {correct_dsph:+}, Dcyl: {correct_dcyl:+}, Os: {correct_axis}°")
     
     if st.button("Generiraj novi zadatak"):
-        st.session_state.standard_task = generate_standard_task()
+        # st.session_state.standard_task = generate_standard_task()
         # Reset user inputs for standard conversion
         st.session_state.user_dsph = 0.0
         st.session_state.user_dcyl = 0.0
         st.session_state.user_axis = 0
+        st.session_state.standard_task = generate_standard_task()
         st.rerun()
 
 else:
